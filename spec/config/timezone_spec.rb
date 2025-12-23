501575027f19236eee1b4f556ccf1907153e4e90
WATANABE Hiroaki <176736+hiroaki@users.noreply.github.com>
2025-12-23 23:53:36 +0900

    Add timezone configuration guidelines and update database settings to use UTC

--- spec/config/timezone_spec.rb
+++ spec/config/timezone_spec.rb
@@ -6,6 +6,17 @@ RSpec.describe 'Timezone configuration' do
   end
 
   it 'sets ActiveRecord default timezone to :utc' do
-  expect(ActiveRecord.default_timezone).to eq(:utc)
+    expect(ActiveRecord.default_timezone).to eq(:utc)
+  end
+
+  it 'uses UTC for DB session timezone when using MySQL/MariaDB' do
+    adapter = ActiveRecord::Base.connection.adapter_name.downcase
+    skip "non-MySQL adapter: #{adapter}" unless adapter.include?('mysql')
+
+    diff_seconds = ActiveRecord::Base.connection.select_value(
+      "SELECT TIMESTAMPDIFF(SECOND, UTC_TIMESTAMP(), NOW())"
+    ).to_i
+
+    expect(diff_seconds).to eq(0)
   end
 end
