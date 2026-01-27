e77742e99c7a28df78be488fe341a9d3ed02d213
WATANABE Hiroaki <176736+hiroaki@users.noreply.github.com>
2026-01-27 21:21:04 +0900

    Refactor bin/dev and related setup:
    - Rename and revise Procfile to Procfile.dev
    - Update bin/dev to use foreman
    - Set default RAILS_ENV in entrypoint.sh
    - Improve container environment configuration

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f `dirname $0`/tmp/pids/server.pid
 
+RAILS_ENV=${RAILS_ENV:-development}
+export RAILS_ENV
+
 echo "entrypoint.sh: RAILS_ENV=$RAILS_ENV"
 # if [ "${RAILS_ENV}" = "production" ]; then
 #   bundle exec rails assets:precompile
@@ -11,4 +14,3 @@ echo "entrypoint.sh: RAILS_ENV=$RAILS_ENV"
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
-
